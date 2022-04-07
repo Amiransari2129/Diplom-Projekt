a079596f477174463fde15bd05a7ebc7e9bc7857
Amiransari2129 <zvkg8wzr8yfty>
2022-04-07 16:04:15 +0200

    Upgrade to react 18

--- client/src/index.js
+++ client/src/index.js
@@ -2,23 +2,25 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
+
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import reducers from './reducers/index';
 
 import './index.css';
 
+const container = document.getElementById('root');
+
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
-ReactDOM.render(
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
